ad3df978799ee49aeb6969c7de6074de0a0b6145
alexdevvv <alex.dev.sergeev@gmail.com>
2021-03-12 14:06:22 +0300

    1.6. Взаимодействие объектов [#481238]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void run(Object object) {
+    }
+
+    public void theEnd() {
+        System.out.println("THE END");
+    }
+}
---
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+public class BallStory {
+    public static void main(String[] args) {
+        Ball ball = new Ball();
+        Hare hare = new Hare();
+        Wolf2 wolf = new Wolf2();
+        Fox fox = new Fox();
+
+        hare.tryEat(ball);
+        ball.run(hare);
+        wolf.tryEat(ball);
+        ball.run(wolf);
+        fox.tryEat(ball);
+        ball.theEnd();
+
+
+    }
+}
