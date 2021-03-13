eb249a248591c240eaa2e6167b73f33ecf726bcd
alexdevvv <alex.dev.sergeev@gmail.com>
2021-03-13 14:38:36 +0300

    2.2. Рефакторинг - Площадь треугольника. [#481231] (v1.2)

---
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void when00And02then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double rsl = a.distance(b);
+        assertThat(rsl, is(2.0));
+    }
+
+    @Test
+    public void when00And010then10() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 10);
+        double rsl = a.distance(b);
+        assertThat(rsl, is(10.0));
+    }
+}
