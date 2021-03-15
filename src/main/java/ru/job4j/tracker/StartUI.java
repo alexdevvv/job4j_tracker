3c8a762917c6f5b5f39e8f687649329cac8b968e
alexdevvv <alex.dev.sergeev@gmail.com>
2021-03-15 18:36:19 +0300

    7. Date. Отображение даты. [#481250] (V1.1)

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,6 +7,6 @@ public class StartUI {
         Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = item.getCreated().format(formatter);
-        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+        System.out.println("Текущие дата и время: " + currentDateTimeFormat);
     }
 }
