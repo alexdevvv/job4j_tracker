80fc9b72fb188efc27e623247afbe186bbb2cc0e
alexdevvv <alex.dev.sergeev@gmail.com>
2021-03-17 20:45:48 +0300

    6. Метод замены заявки. Tracker.replace [#481258]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,10 +3,15 @@ package ru.job4j.tracker;
 public class StartUI {
     public static void main(String[] args) {
         Tracker tracker = new Tracker();
-        Item item = new Item();
-        item.setName("ABCD");
-        tracker.add(item);
+        Item item1 = new Item();
+        Item item2 = new Item();
+        item1.setName("ABCD");
+        item2.setName("1234");
+        tracker.add(item1);
+        tracker.add(item2);
 
         System.out.println(tracker.findById(1).getName());
+        System.out.println(tracker.findById(2).getName());
+
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -30,15 +30,33 @@ public class Tracker {
         return newNameItem;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
     }
+
+    public Item findById(int id) {
+        /* Находим индекс */
+        int index = indexOf(id);
+        /* Если индекс найден возвращаем item, иначе null */
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        boolean x = false;
+        int index = indexOf(id);
+        if (index != -1) {
+            int y = items[index].getId();
+            items[index] = item;
+            item.setId(y);
+            x = true;
+        } return x;
+    }
 }
+
