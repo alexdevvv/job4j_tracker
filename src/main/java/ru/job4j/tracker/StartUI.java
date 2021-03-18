ab2abf15a400d25031fa4fa642dceca85c012340
alexdevvv <alex.dev.sergeev@gmail.com>
2021-03-18 16:41:47 +0300

    9. toString [#481262]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,18 +2,8 @@ package ru.job4j.tracker;
 
 public class StartUI {
     public static void main(String[] args) {
-        Tracker tracker = new Tracker();
-        Item item1 = new Item();
-        Item item2 = new Item();
-        item1.setName("ABCD");
-        item2.setName("1234");
-        tracker.add(item1);
-        tracker.add(item2);
-
-        System.out.println(tracker.delete(1));
-//
-//        System.out.println(tracker.findById(1).getName());
-        System.out.println(tracker.findById(2).getName());
+        Item item = new Item();
+        System.out.println(item);
 
     }
 }
