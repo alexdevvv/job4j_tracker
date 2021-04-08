80bed30a551dd5c18d173f9dfafa58d9482bd5c3
Alex Sergeev <alex.dev.sergeev@gmail.com>
2021-04-08 10:43:32 +0300

    0. Коллекции, ArrayList, List, Обобщения [#481168]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 
 public class UsageArrayList {
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<String>();
+        ArrayList<String> names = new ArrayList<>();
         names.add("Petr");
         names.add("Ivan");
         names.add("Stepan");
 
-        for (Object obj : names) {
+        for (String obj : names) {
             System.out.println(obj);
         }
     }
