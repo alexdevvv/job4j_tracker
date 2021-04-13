0fcfb78c50a4ffc93949b5baa0c567a1392853c1
Alex Sergeev <alex.dev.sergeev@gmail.com>
2021-04-13 14:31:50 +0300

    0. Map, HashMap [#481170]

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,28 @@
+package ru.job4j.collection;
+
+import java.util.Map;
+
+import java.util.HashMap;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean rsl = false;
+        if (!citizens.containsKey(citizen.getPassport())) {
+            citizens.put(citizen.getPassport(), citizen);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public Citizen get(String passport) {
+        Citizen citizen = null;
+       if (citizens.containsKey(passport)) {
+           citizen = citizens.get(passport);
+       }
+     return citizen;
+    }
+
+
+}
--- src/main/java/ru/job4j/collection/UserMap.java
+++ src/main/java/ru/job4j/collection/UserMap.java
@@ -1,17 +1,21 @@
 package ru.job4j.collection;
 
+import java.util.*;
 import java.util.HashMap;
-import java.util.Map;
 
 public class UserMap {
     public static void main(String[] args) {
         Map<String, String> userDate = new HashMap<>();
         userDate.put("aaa@gmail.com", "Donald Trump");
         userDate.put("cng@gmail.com", "Ivan Ivanov");
+        userDate.put("cngg@gmail.com", "Ivan Sidorov");
+        userDate.put("cnggsad@gmail.com", "Gnida");
+        userDate.put("cnggfd@gmail.com", "Semen");
 
-        for (String mail : userDate.keySet()) {
-            String value = userDate.get(mail);
-            System.out.println(mail + " " + value);
+        for (Map.Entry entry : userDate.entrySet()) {
+
+            System.out.println("Key - " + entry.getKey() + " " + entry.getValue());
+
         }
 
     }
