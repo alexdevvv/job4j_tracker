1cce48841675e3b79cffd003546e6e6fa5025527
alexdevvv <alex.dev.sergeev@gmail.com>
2021-04-19 21:47:43 +0300

    2. Job + Comparators [#481176]

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,79 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertThat;
+
+public class JobTest {
+    @Test
+    public void whenComparatorNameAscending() {
+        Job job1 = new Job("BBB", 111);
+        Job job2 = new Job("CCC", 222);
+        Job job3 = new Job("AAA", 222);
+        List<Job> list = new ArrayList<>();
+        list.add(job1);
+        list.add(job2);
+        list.add(job3);
+        List<Job> expect = new ArrayList<>();
+        expect.add(job3);
+        expect.add(job1);
+        expect.add(job2);
+        Collections.sort(list, new NameAscendingComparator());
+        assertThat(expect, is(list));
+    }
+
+    @Test
+    public void whenComparatorNameDescending() {
+        Job job1 = new Job("BBB", 111);
+        Job job2 = new Job("CCC", 222);
+        Job job3 = new Job("AAA", 222);
+        List<Job> list = new ArrayList<>();
+        list.add(job1);
+        list.add(job2);
+        list.add(job3);
+        List<Job> expect = new ArrayList<>();
+        expect.add(job2);
+        expect.add(job1);
+        expect.add(job3);
+        Collections.sort(list, new NameDescendingComparator());
+        assertThat(expect, is(list));
+    }
+
+    @Test
+    public void whenComparatorPriorityAscending() {
+        Job job1 = new Job("BBB", 222);
+        Job job2 = new Job("CCC", 444);
+        Job job3 = new Job("AAA", 333);
+        List<Job> list = new ArrayList<>();
+        list.add(job1);
+        list.add(job2);
+        list.add(job3);
+        List<Job> expect = new ArrayList<>();
+        expect.add(job1);
+        expect.add(job3);
+        expect.add(job2);
+        Collections.sort(list, new PriorityAscendingComparator());
+        assertThat(expect, is(list));
+    }
+
+    @Test
+    public void whenComparatorPriorityDescending() {
+        Job job1 = new Job("BBB", 222);
+        Job job2 = new Job("CCC", 444);
+        Job job3 = new Job("AAA", 333);
+        List<Job> list = new ArrayList<>();
+        list.add(job1);
+        list.add(job2);
+        list.add(job3);
+        List<Job> expect = new ArrayList<>();
+        expect.add(job2);
+        expect.add(job3);
+        expect.add(job1);
+        Collections.sort(list, new PriorityDescendingComparator());
+        assertThat(expect, is(list));
+    }
+}
