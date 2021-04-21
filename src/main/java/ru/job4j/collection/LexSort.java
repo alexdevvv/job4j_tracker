e308de3ed2e2d6594c8b72182f63510a01236361
Alex Sergeev <alex.dev.sergeev@gmail.com>
2021-04-21 11:51:07 +0300

    4. Сортировка номера [#481157]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String[] arrayChar1 = o1.split("\\.");
+        String[] arrayChar2 = o2.split("\\.");
+        int rsl1 = Integer.parseInt(arrayChar1[0]);
+        int rsl2 = Integer.parseInt(arrayChar2[0]);
+        return Integer.compare(rsl1, rsl2);
+    }
+}
