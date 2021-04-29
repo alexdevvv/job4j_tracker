655b8088101da372b3d1d94e692383da0084c13f
Alex Sergeev <alex.dev.sergeev@gmail.com>
2021-04-29 09:45:14 +0300

    2.2. Лямбда блок [#480987]

--- src/main/java/ru/job4j/lambda/Attachment.java
+++ src/main/java/ru/job4j/lambda/Attachment.java
@@ -19,9 +19,6 @@ public class Attachment {
 
     @Override
     public String toString() {
-        return "Attachment{" +
-                "name='" + name + '\'' +
-                ", size=" + size +
-                '}';
+        return "Attachment{" + "name='" + name + '\'' + ", size=" + size + '}';
     }
 }
