1d1e66284ec86424fff0e13a6f8e86db94df9d4d
Alex Sergeev <alex.dev.sergeev@gmail.com>
2021-05-07 13:22:36 +0300

    4. Преобразование List в Map. [#480998]

--- src/main/java/ru/job4j/stream/MapStudents.java
+++ src/main/java/ru/job4j/stream/MapStudents.java
@@ -1,19 +1,25 @@
 package ru.job4j.stream;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
+import java.util.TreeMap;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 
-//public class MapStudents {
-//    public Map<String, Student> createMapStudents(List<Student> listStudents) {
-//        return listStudents.stream()
-//                .distinct()
-//                .collect(
-//                        Collectors.toMap(
-//                             Student::getSurname,
-//                             s -> getClass())
-//                );
-//
-//    }
-//}
+public class MapStudents {
+    public Map<String, Student> createMapStudents(List<Student> listStudents) {
+        return listStudents.stream()
+                .distinct()
+                .collect(
+                        Collectors.toMap(
+                             Student::getSurname,
+                             s -> new Student(s.getScore(), s.getSurname()),
+                                (s1, s2) -> {
+                                 return s1.equals(s2) ? s1 : s2;
+                                }
+                )
+        );
+    }
+}
--- src/test/java/ru/job4j/stream/MapStudentsTest.java
+++ src/test/java/ru/job4j/stream/MapStudentsTest.java
@@ -6,26 +6,30 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class MapStudentsTest {
-//
-//    @Test
-//    public void createMapStudents() {
-//        List<Student> listStudent = Arrays.asList(
-//                new Student(10, "Vasin"),
-//                new Student(95, "Feldman"),
-//                new Student(34, "Zabolotny"),
-//                new Student(58, "Makarov")
-//        );
-//        Map<String, Student> expect = new HashMap<>();
-//        expect.put("Feldman", new Student(95, "Feldman"));
-//        expect.put("Makarov", new Student(58, "Makarov"));
-//        MapStudents ms = new MapStudents();
-//        Map<String, Student> rsl = ms.createMapStudents(listStudent);
-//        assertThat(expect, is(rsl));
-//
-//    }
+
+    @Test
+    public void createMapStudents() {
+        List<Student> listStudent = Arrays.asList(
+                new Student(1, "Vasin"),
+                new Student(1, "Vasin"),
+                new Student(89, "Feldman"),
+                new Student(10, "Zabolotny"),
+                new Student(11, "Zabolotny"),
+                new Student(19, "Buzova"),
+                new Student(98, "Makarov")
+        );
+        Map<String, Student> expect = new HashMap<>();
+        expect.put("Feldman", new Student(95, "Feldman"));
+        expect.put("Makarov", new Student(98, "Makarov"));
+        MapStudents ms = new MapStudents();
+        Map<String , Student> rsl = ms.createMapStudents(listStudent);
+        assertThat(rsl.size(), is(5));
+
+    }
 }
