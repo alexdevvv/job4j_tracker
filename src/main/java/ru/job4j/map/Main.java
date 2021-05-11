2c7fa9c796b6faf3119c2d2556b71680227e0461
Alex Sergeev <alex.dev.sergeev@gmail.com>
2021-05-11 15:07:19 +0300

    College

---
+++ src/main/java/ru/job4j/map/Main.java
@@ -0,0 +1,21 @@
+package ru.job4j.map;
+
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Student, Set<Subject>> student = Map.of(new Student("Student", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Algebra", 95),
+                        new Subject("Development Java", 91),
+                        new Subject("English", 58))
+        );
+
+        College college = new College(student);
+        Student rsl = college.findByAccount("000001");
+        System.out.println(rsl);
+        Subject sbj = college.findBySubjectName("000001", "Development Java");
+        System.out.println(sbj);
+    }
+}
